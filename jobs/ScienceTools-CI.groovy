2b4b47bf48d232936dcf50ce43ca16b541eca897
Alex Reustle <Areustle@users.noreply.github.com>
2018-01-29 15:24:42 -0500

    U/areustle/delete dir (#8)
    * Clean Directory as first step. Use rm -rf *

--- jobs/ScienceTools-CI.groovy
+++ jobs/ScienceTools-CI.groovy
@@ -37,6 +37,7 @@ try {
             // Create a map to pass in to the 'parallel' step so we can fire all the builds at once
             builders[buildNode] = {
                 node(buildNode) {
+                    sh 'rm -rf *'
                     sh "source /scratch/bvan/repoman-env/bin/activate && repoman checkout --force --develop ${project} ${repoman_ref}"
                 }
             }
